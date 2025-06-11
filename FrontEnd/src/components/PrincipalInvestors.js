6b75615f9b8fcb4282dfce45b4fce8f0feea3cba
vevelle-95 <152355711+vevelle-95@users.noreply.github.com>
2025-06-11 21:04:58 +0800

    Update PrincipalInvestors.js
    added and updated attributes

--- FrontEnd/src/components/PrincipalInvestors.js
+++ FrontEnd/src/components/PrincipalInvestors.js
@@ -7,8 +7,25 @@ function PrincipalInvestors() {
     Accnt_ID: '',
     Princip_Investor_ID: '',
     Princip_Investor_Name: '',
+    Princip_Investor_Perma_Add: '',
+    Princip_Investor_Present_Add: '',
+    Princip_Investor_HomeNo: '',
     Princip_Investor_Birth_Date: '',
     Princip_Investor_Nationality: '',
+    Princip_Investor_Sex: '',
+    Princip_Investor_Civil_Status: '',
+    Princip_Investor_Birth_Place: '',
+    Princip_Investor_Email_Add: '',
+    SSS_No: '',
+    Princip_Investor_WorkNo: '',
+    Princip_Investor_Occupation: '',
+    Nature_Work: '',
+    Job_Description: '',
+    Company_Name: '',
+    Gross_Annual_Income: '',
+    Princip_Investor_Work_Address: '',
+    Princip_Investor_Mailing_Address: '',
+    PH_TIN: '',
   });
 
   useEffect(() => {
@@ -33,8 +50,25 @@ function PrincipalInvestors() {
         Accnt_ID: '',
         Princip_Investor_ID: '',
         Princip_Investor_Name: '',
+        Princip_Investor_Perma_Add: '',
+        Princip_Investor_Present_Add: '',
+        Princip_Investor_HomeNo: '',
         Princip_Investor_Birth_Date: '',
         Princip_Investor_Nationality: '',
+        Princip_Investor_Sex: '',
+        Princip_Investor_Civil_Status: '',
+        Princip_Investor_Birth_Place: '',
+        Princip_Investor_Email_Add: '',
+        SSS_No: '',
+        Princip_Investor_WorkNo: '',
+        Princip_Investor_Occupation: '',
+        Nature_Work: '',
+        Job_Description: '',
+        Company_Name: '',
+        Gross_Annual_Income: '',
+        Princip_Investor_Work_Address: '',
+        Princip_Investor_Mailing_Address: '',
+        PH_TIN: '',
       });
     } catch (err) {
       console.error(err);
@@ -54,6 +88,7 @@ function PrincipalInvestors() {
           />
         ))}
         <button type="submit">Add</button>
+        <button type="remove">Delete</button>
       </form>
 
       <table border="1" cellPadding="5">
@@ -62,8 +97,25 @@ function PrincipalInvestors() {
             <th>Account ID</th>
             <th>Investor ID</th>
             <th>Name</th>
+            <th>Permanent Address</th>
+            <th>Present Address</th>
+            <th>Home No</th>
             <th>Birth Date</th>
             <th>Nationality</th>
+            <th>Sex</th>
+            <th>Civil Status</th>
+            <th>Birth Place</th>
+            <th>Email Address</th>
+            <th>SSS No</th>
+            <th>Work No</th>
+            <th>Occupation</th>
+            <th>Nature of Work</th>
+            <th>Job Description</th>
+            <th>Company Name</th>
+            <th>Gross Annual Income</th> 
+            <th>Work Address</th>
+            <th>Mailing Address</th>
+            <th>PH TIN No.</th>
           </tr>
         </thead>
         <tbody>
@@ -72,8 +124,25 @@ function PrincipalInvestors() {
               <td>{inv.Accnt_ID}</td>
               <td>{inv.Princip_Investor_ID}</td>
               <td>{inv.Princip_Investor_Name}</td>
+              <td>{inv.Princip_Investor_Perma_Add}</td>
+              <td>{inv.Princip_Investor_Present_Add}</td>
+              <td>{inv.Princip_Investor_HomeNo}</td>
               <td>{inv.Princip_Investor_Birth_Date}</td>
               <td>{inv.Princip_Investor_Nationality}</td>
+              <td>{inv.Princip_Investor_Sex}</td>
+              <td>{inv.Princip_Investor_Civil_Status}</td>
+              <td>{inv.Princip_Investor_Birth_Place}</td>
+              <td>{inv.Princip_Investor_Email_Add}</td>
+              <td>{inv.SSS_No}</td>
+              <td>{inv.Princip_Investor_WorkNo}</td>
+              <td>{inv.Princip_Investor_Occupation}</td>
+              <td>{inv.Nature_Work}</td> 
+              <td>{inv.Job_Description}</td>
+              <td>{inv.Company_Name}</td>
+              <td>{inv.Gross_Annual_Income}</td>
+              <td>{inv.Princip_Investor_Work_Address}</td>
+              <td>{inv.Princip_Investor_Mailing_Address}</td>
+              <td>{inv.PH_TIN}</td>
             </tr>
           ))}
         </tbody>
