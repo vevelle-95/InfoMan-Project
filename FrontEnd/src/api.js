0ae354ac6d53c38a095442ff9745bad29ca6b10b
JRMaps <149369368+JRMaps@users.noreply.github.com>
2025-06-11 15:24:51 +0800

    Updated for local hosting
    local host change form link to local host:5000

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://infomang11.ddns.net:5000'; // adjust if needed
+const API_BASE = 'localhost:5000'; // adjsuted for local development
 
 export const api = axios.create({
   baseURL: API_BASE,
