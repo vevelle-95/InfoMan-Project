61f83355bf9a1e78fc51ba00af73e1de40178468
vevelle-95 <152355711+vevelle-95@users.noreply.github.com>
2025-06-12 23:42:59 +0800

    Update TPBOs.js
    update date format

--- FrontEnd/src/components/TPBOs.js
+++ FrontEnd/src/components/TPBOs.js
@@ -170,9 +170,7 @@ function TPBOs() {
               <td>{tpbo.TPBO_Name}</td>
               <td>{tpbo.TPBO_Relationship}</td>
               <td>{tpbo.TPBO_Residence}</td>
-              <td>{tpbo.TPBO_Birth_Date && !isNaN(new Date(tpbo.TPBO_Birth_Date))
-                ? new Date(tpbo.TPBO_Birth_Date).toISOString().slice(0, 10)
-                : ''}</td>
+              <td>{tpbo.TPBO_Birth_Date && !isNaN(new Date(tpbo.TPBO_Birth_Date)) ? new Date(date).toLocaleDateString('en-CA'): ''}</td>
               <td>{tpbo.TPBO_Birth_Place}</td>
               <td>{tpbo.TPBO_Sex}</td>
               <td>{tpbo.TPBO_TIN}</td>
