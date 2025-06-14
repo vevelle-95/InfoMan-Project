db4a612e90fe351865e9bbffaac663e2eb2e3db6
vevelle-95 <152355711+vevelle-95@users.noreply.github.com>
2025-06-14 11:33:00 +0800

    Update index.jsx

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.css';
-import './styles/FormStyles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
